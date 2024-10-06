37ecb24e5c1b66296a2fd19e50a65ee9bb60b278
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-10-07 01:34:10 +0800

    commit 242

--- hello.c
+++ hello.c
@@ -6,14 +6,14 @@ int main(){
     // float f=3.14;
     // char* s = "hello";
     // printf("%s i=%d f=%d\n", s, i;, i);
-    int x =0;
-    int y = 1;
-    x = ++y;
+    // int x =0;
+    // int y = 1;
+    // x = ++y;
 
-    printf("x: %d, y: %d\n", x, y);
+    // printf("x: %d, y: %d\n", x, y);
 
-    x = y++;
-    printf("x: %d, y: %d\n", x, y);
+    // x = y++;
+    // printf("x: %d, y: %d\n", x, y);
     // y>x?printf("y is greater than x"):printf("y is not greater than x");
     // bool b = true;
     // if (b){
@@ -21,5 +21,17 @@ int main(){
     // }
     // int z=4;
     // printf("%s\n", z%2==0?"even":"odd");
+    // int x;
+    // x = (1, 2, 3);
+    // printf("%d\n", x);
+    
+
+    
+
+    for (int i=0,j=0; i<10; i++)
+    {
+        printf("i is :%d\n, j is %d\n", i,j);
+    
+    }
 
 }

c72c3b6718abf35d2f893e44280d0b007e453ad9
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-10-07 01:07:57 +0800

    commit 240

--- hello.c
+++ hello.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
-    int i =2;
-    float f=3.14;
-    char* s = "hello";
-    printf("%s i=%d f=%d\n", s, i, i);
+    // int i =2;
+    // float f=3.14;
+    // char* s = "hello";
+    // printf("%s i=%d f=%d\n", s, i;, i);
+    int x =0;
+    int y = 1;
+    // y>x?printf("y is greater than x"):printf("y is not greater than x");
+    // bool b = true;
+    // if (b){
+    //     printf("b is true");
+    // }
+    int z=4;
+    printf("%s\n", z%2==0?"even":"odd");
+
 }
